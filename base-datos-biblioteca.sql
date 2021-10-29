4eb37374bdc9bcd61dcbbf5d014de694fa7f41b2
Jesus Martinez <jesusayala368@gmail.com>
2021-10-29 00:21:03 -0500

    bade de datos biblioteca

---
+++ base-datos-biblioteca.sql
@@ -0,0 +1,37 @@
+# create #
+
+# tabla clientes #
+
+INSERT INTO clientes VALUES(NULL, 'Jesus', 'Estado de Mexcio', '54793254', 'email@gmail.com'); 
+INSERT INTO clientes VALUES(NULL, 'Vin', 'cdmx', '54526872', 'prueba@gmail.com');
+INSERT INTO clientes VALUES(NULL, 'Mia', 'Pachuca', '481355155', 'prueba2@gmail.com');
+INSERT INTO clientes VALUES(NULL, 'Brian', 'cdmx', '687314592', 'prueba3@gmail.com');
+INSERT INTO clientes VALUES(NULL, 'Letty', 'cdmx', '974681354', 'prueba4@gmail.com');
+
+# tabla autores #
+
+INSERT INTO autores VALUES(NULL, 'Dom', 'Mexicana');
+INSERT INTO autores VALUES(NULL, 'William Shakespeare', 'Ingles');
+INSERT INTO autores VALUES(NULL, 'Gabriel García Márquez', 'Mexicana');
+INSERT InTO Autores VALUES(NULL, 'Haruki Murakami', 'Japones');
+INSERT InTO Autores VALUES(NULL, 'Charles Darwin', 'EEUU');
+
+# tabla libros #
+
+INSERT INTO Libros VALUES(NULL, 1, 'La magia', '2002', '54783209', '1993/02/05', NULL);
+INSERT INTO Libros VALUES(NULL, 2, 'La magia', '2002', '54783209', '1993/02/05', NULL);
+INSERT INTO Libros VALUES(NULL, 3, 'La magia', '2002', '54783209', '1993/02/05', NULL);
+INSERT INTO Libros VALUES(NULL, 4, 'La magia', '2002', '54783209', '1993/02/05', NULL);
+INSERT INTO Libros VALUES(NULL, 5, 'La magia', '2002', '54783209', '1993/02/05', NULL);
+
+# tabla rentas #
+
+INSERT INTO Rentas VALUES(NULL, 1, 1, '2005/09/06', '2005/09/15', 'polanco');
+INSERT INTO Rentas VALUES(NULL, 2, 2, '2005/09/06', '2005/09/15', 'polanco');
+INSERT INTO Rentas VALUES(NULL, 3, 3, '2005/09/06', '2005/09/15', 'polanco');
+INSERT INTO Rentas VALUES(NULL, 4, 4, '2005/09/06', '2005/09/15', 'polanco');
+INSERT INTO Rentas VALUES(NULL, 5, 5, '2005/09/06', '2005/09/15', 'polanco');
+
+
+
+
